a1af1264e22d704da9383ec1ba7c9015a8f40b10
gilthonwe <gilthonwe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-07 15:26:31 +0000

    version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/appstore-reviews-viewer/tags/1.0.3@1176273 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/appstore-review.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function() {
+	var speed = 10000;
+	
+	var slides = jQuery('.asrv_review');
+	var slideIndex = -1;
+	
+	function showNextSlide() {
+		++slideIndex;
+		slides.eq(slideIndex % slides.length)
+			.fadeIn(1000)
+			.delay(speed)
+			.fadeOut(1000, showNextSlide);
+	}
+	
+	if (slides.length == 1) {
+		slides.show();
+	} else {
+		showNextSlide();
+	}
+});
